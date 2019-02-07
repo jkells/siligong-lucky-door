f2d1a9627400f193659958dae7667787345c50b5
Jared Kells <jared@easyagile.com>
2019-02-08 09:04:39 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Siligong JS 2.1 Lucky Door Prize Giveaway!
+
+You need to use iTerm and provide your own signed meetup URL.
---
+++ luckyDoor.js
@@ -0,0 +1,52 @@
+const _ = require("lodash")
+const fetch = require("node-fetch")
+const clear = require("clear")
+const termImg = require("term-img")
+
+const getImage = async rsvp => {
+    const url = rsvp.member.photo ? rsvp.member.photo.photo_link : "https://secure.meetupstatic.com/photos/event/8/a/b/3/600_470975507.jpeg"
+    const imgResponse = await fetch(url)
+    return termImg.string(await imgResponse.buffer(), { height: "60%" })
+}
+
+const doWelcome = async (length) => {
+    clear()
+    console.log()
+    console.log("Siligong.js Lucky Door prize generator")
+    console.log(`Picking a winner out of ${length} attendees`)
+    await sleep(5000)
+    clear()
+    console.log()
+}
+
+const sleep = (x) => new Promise((resolve) => setTimeout(() => resolve(), x))
+
+const luckyDoor = async () => {
+    // You'll need a pre-signed URL for this to work, I've deleted the signature.
+    // Use this: https://secure.meetup.com/meetup_api/console/?path=/:urlname/events/:event_id/rsvps
+    const response = await fetch("https://api.meetup.com/SiligongValley/events/257181487/rsvps?photo-host=public&response=yes")
+    const rsvps = _.shuffle(await response.json())
+    await doWelcome(rsvps.length)
+    const images = await Promise.all(rsvps.map(getImage))
+
+    for (var i = 0; i < rsvps.length * 2; i++) {
+        const winner = rsvps[i % rsvps.length]
+        const photo = images[i % rsvps.length]
+
+        clear()
+        console.log()
+        console.log(photo)
+        console.log(winner.member.name)
+
+        await sleep(Math.pow(i, 3) / 3000 + 500)
+    }
+
+    clear()
+    console.log()
+    const winner = rsvps[i % rsvps.length]
+    const photo = images[i % rsvps.length]
+    console.log(photo)
+    console.error("THE WINNER IS:", winner.member.name, "!!!!!")
+}
+
+luckyDoor()
